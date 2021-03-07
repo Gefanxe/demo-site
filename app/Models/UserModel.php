6b4cb2963856e1e38ae3e32a0409a47ba2bf9c3b
Andersen <actfire@gmail.com>
2021-03-07 17:00:49 +0800

    update

---
+++ app/Models/UserModel.php
@@ -0,0 +1,44 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table      = 'users';
+    protected $primaryKey = 'id';
+
+    protected $returnType = 'App\Entities\User';
+    protected $useSoftDeletes = true;
+
+    // 准許進事件的key(跟資料庫欄位無關)
+    protected $allowedFields = ['email', 'password'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules = [
+        'email' => 'required|valid_email|is_unique[mng_users.email]',
+        'password' => 'required|min_length[8]',
+        'pass_confirm' => 'required_with[password]|matches[password]'
+    ];
+
+    protected $validationMessages = [
+        'email' => [
+            'is_unique' => 'Email不能重複哦'
+        ]
+    ];
+    protected $skipValidation = false;
+
+    protected function hashPassword(array $data)
+    {
+        if (! isset($data['data']['password'])) return $data;
+
+        $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_BCRYPT);
+
+        return $data;
+    }
+
+    protected $beforeInsert = ['hashPassword'];
+}
